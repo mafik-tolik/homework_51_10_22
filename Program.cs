c0ab072af5bd460a1df8ec8152c8433a44ca05c8
mafik_tolik <mafik_tolik@mail.ru>
2022-10-05 11:14:38 +0300

    Finish

--- Program.cs
+++ Program.cs
@@ -1,7 +1,8 @@
 ﻿Console.Clear();
 
 
-// Ex41();
+
+Ex41();
 // Задача 41: Пользователь вводит с клавиатуры M чисел. Посчитайте, сколько чисел больше 0 ввёл пользователь.
 // 0, 7, 8, -2, -2 -> 2
 // 1, -7, 567, 89, 223-> 3
@@ -42,5 +43,24 @@ void Ex41()
 
 
 
+Ex43();
+// Задача 43: Напишите программу, которая найдёт точку пересечения двух прямых, заданных уравнениями y = k1 * x + b1, y = k2 * x + b2; 
+// значения b1, k1, b2 и k2 задаются пользователем.
+// b1 = 2, k1 = 5, b2 = 4, k2 = 9 -> (-0,5; -0,5)
 
+void Ex43()
+{
+    Console.WriteLine("Введите k1");
+    double k1 = Convert.ToInt32(Console.ReadLine());
+    Console.WriteLine("Введите b1");
+    double b1 = Convert.ToInt32(Console.ReadLine());
+    Console.WriteLine("Введите k2");
+    double k2 = Convert.ToInt32(Console.ReadLine());
+    Console.WriteLine("Введите b2");
+    double b2 = Convert.ToInt32(Console.ReadLine());
 
+    double x = (b2 - b1) / (k1 - k2);
+    double y1 = k1*x + b1;
+
+    Console.WriteLine($"Точка пересечения двух прямых: ({x}; {y1})");
+}
